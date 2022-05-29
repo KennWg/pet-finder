36d1a44a2c7aed7693f578feec7a0c26940ae9db
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-29 17:07:51 -0400

    GQL server fixes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
 // import the gql tagged template function
 
-const {gql} = require ('apollo-server-express')
+const { gql } = require('apollo-server-express')
 
 // create out typeDefs
 const typeDefs = gql`
@@ -25,37 +25,47 @@ type Report {
 }
 
 type Comment {
-    commentId: ID
-    report: Report
-    user: User
-    commentBody: String
-    createdAt: String
+  commentId: ID
+  report: Report
+  user: User
+  commentBody: String
+  createdAt: String
 }
 
 type Auth {
-    token: ID!
-    user: User
-  }
+  token: ID!
+  user: User
+}
 
 type Query {
-    allReports = [Report]
-    report: Report
-    reportsByUser(createdBy: ID!): [Report]
-    reportsByUserComments (user: ID!, report: ID!, commentId: ID!): [Report]
-  }
-  
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!, address: String!): Auth
-    addReport(name: String!, breed: String, photo: String!, description: String, lastSeen: String!, createdAt: String!, createdBy: ID!): Report
-    updateReport(name: String!, breed: String, photo: String!, description: String, lastSeen: String!, createdAt: String!, createdBy: ID!): Report
-    deleteReport(_id: ID!): Report
-    addComment(report: ID!, user: ID!, commentBody: String!, createdAt: String!): Report
+  reports: [Report]
+  report: Report
+  reportComments(user: ID!, report: ID!, commentId: ID!): [Report]
+}
 
-  }
-  
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!, address: String!): Auth
+  addReport(name: String!, breed: String, photo: String!, description: String, lastSeen: String!, createdAt: String!, createdBy: ID!): Report
+  updateReport(name: String!, breed: String, photo: String!, description: String, lastSeen: String!, createdAt: String!, createdBy: ID!): Report
+  deleteReport(_id: ID!): Report
+  addComment(report: ID!, user: ID!, commentBody: String!, createdAt: String!): Report
+
+}
+
   `;
 
+
+
+
+
+// REMOVED when refactoring
+// type Query {
+//   allReports: [Report]
+//   report: Report
+//   reportsByUser(createdBy: ID!): [Report]
+//   reportsByUserComments (user: ID!, report: ID!, commentId: ID!): [Report]
+// }
 
 // export typdefs
 module.exports = typeDefs;
