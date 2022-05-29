d367878d3130cd85d35c01291279c5a6840bc70f
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-05-29 19:24:09 -0400

    modify the tablet.js and mobile.js

--- client/src/components/Navbar/Tablet.js
+++ client/src/components/Navbar/Tablet.js
@@ -1,10 +1,12 @@
 import { useStoreContext } from '../../utils/GlobalStore';
 import { UPDATE_VIEW } from '../../utils/actions';
 
+import Auth from '../../utils/auth.js';
+
 function Tablet() {
 
     const [state, dispatch] = useStoreContext();
-    const { currentView, navBarChoices } = state;
+    const { currentView, navBarChoices, navBarChoicesNOT } = state;
 
     const imagePath = imageName => {
         return require(`../../assets/images/${imageName}.png`)
@@ -19,28 +21,45 @@ function Tablet() {
 
 
     return (
-        <div className="tablet-navbar">
-            {navBarChoices.map((view) => (
-                <div
-                    className={`tablet-nav-div`}
-                    key={view.name}
-                >
-                    <a className={`nav-link ${currentView === view.name ? 'active' : ''}`}
-                        onClick={() => {
-                            handleClick(view.name);
+        <>
+            {(Auth.loggedIn())
+                ? <div className="tablet-navbar">
+                    {navBarChoices.map((view) => (
+                        <div className={`tablet-nav-div`}
+                            key={view.name}>
+                            <a className={`nav-link ${currentView === view.name ? 'active' : ''}`}
+                                onClick={() => {
+                                    handleClick(view.name);
+                                }}
+                                href="#"
+                                title={view.description}>
+                                <img src={imagePath(view.name)} className="tablet-nav-icon" alt={view.name} />
+                            </a>
+                        </div>
+                    ))
+                    }
 
-                        }}
+                </div >
 
-                        href="#"
-                        title={view.description}
-                    >
-                        <img src={imagePath(view.name)} className="tablet-nav-icon" alt={view.name} />
-                    </a>
-                </div>
-            ))
-            }
+                : <div className="tablet-navbar">
+                    {navBarChoicesNOT.map((view) => (
+                        <div className={`tablet-nav-div`}
+                            key={view.name}>
+                            <a className={`nav-link ${currentView === view.name ? 'active' : ''}`}
+                                onClick={() => {
+                                    handleClick(view.name);
+                                }}
+                                href="#"
+                                title={view.description}>
+                                <img src={imagePath(view.name)} className="tablet-nav-icon" alt={view.name} />
+                            </a>
+                        </div>
+                    ))
+                    }
 
-        </div >
+                </div >}
+        </>
+
 
     )
 }
