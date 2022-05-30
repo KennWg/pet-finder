a8cdc1e8d295f9759f99e69f6460be8da47c5cbc
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-05-30 16:53:55 -0400

    fix thegraph1l server by commenting out the app.get('*',(req, res) )

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, '../client/public')));
 // }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/public/index.html'));
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
