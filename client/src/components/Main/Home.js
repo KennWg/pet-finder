8af1e108ca3fbfbf6ebe81af92495ae11ab0310f
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-31 09:00:43 -0400

    start of major site styling

--- client/src/components/Main/Home.js
+++ client/src/components/Main/Home.js
@@ -4,11 +4,12 @@ import dogImg from '../../assets/images/dog.jpg';
 function Home() {
     return (
 
-        <div className="home-class outer-div">
+        <div className="home-class outer-div bg-color">
 
-            <h1>Welcome to the Pet Finder</h1>
-            <br></br>
+            <h1 className="brand-title">Welcome to the Pet Finder</h1>
+            <h2 className="brand-text">We'll help you spread the word.</h2>
             <img src={dogImg} alt="a dog sit on the front page" className='homepage-dog' />
+            <br></br>
         </div>
     );
 }
