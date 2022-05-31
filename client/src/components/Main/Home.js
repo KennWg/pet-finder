720881b5cc8adaba68f21c6ff607fa99630300b9
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-31 09:26:25 -0400

    title card styles work

--- client/src/components/Main/Home.js
+++ client/src/components/Main/Home.js
@@ -4,7 +4,7 @@ import dogImg from '../../assets/images/dog.jpg';
 function Home() {
     return (
 
-        <div className="home-class outer-div bg-color">
+        <div className="home-class outer-div brand-bg-color">
 
             <h1 className="brand-title">Welcome to the Pet Finder</h1>
             <h2 className="brand-text">We'll help you spread the word.</h2>
