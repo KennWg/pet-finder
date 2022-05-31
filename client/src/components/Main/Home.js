cd29745b4392565171a27748b18812b6ea7bf51d
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-31 13:06:09 -0400

    still working through styling choices

--- client/src/components/Main/Home.js
+++ client/src/components/Main/Home.js
@@ -5,11 +5,15 @@ function Home() {
     return (
 
         <div className="home-class outer-div brand-bg-color">
+            <div className="inner-div body-bg-color">
+                <h1 className="brand-title">Pet Alert</h1>
+                <h2 className="brand-text">Quickly build a community brigade.</h2>
+                <div className="dog-img-div">
+                <img src={dogImg} alt="a dog sit on the front page" className='homepage-dog' />
+                </div>
+                <br></br>
+            </div>
 
-            <h1 className="brand-title">Pet Alert</h1>
-            <h2 className="brand-text">Quickly build a community brigade.</h2>
-            <img src={dogImg} alt="a dog sit on the front page" className='homepage-dog' />
-            <br></br>
         </div>
     );
 }
