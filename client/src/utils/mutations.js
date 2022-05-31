5a8b10e62f33d7bc0fe271a21d0bafea38813a12
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-30 20:45:58 -0400

    CreateReport.js posts report to server

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,21 +26,10 @@ mutation Mutation($username: String!, $email: String!, $password: String!, $addr
 `
 
 export const CREATE_REPORT = gql`
-mutation Mutation($name: String!, $photo: String!, $lastSeen: String!, $createdBy: ID!, $breed: String, $description: String) {
-  addReport(name: $name, photo: $photo, lastSeen: $lastSeen, createdBy: $createdBy, breed: $breed, description: $description) {
+mutation Mutation($name: String!, $photo: String!, $description: String!, $lastSeen: String!, $breed: String) {
+  addReport(name: $name, photo: $photo, description: $description, lastSeen: $lastSeen, breed: $breed) {
     _id
     name
-    breed
-    photo
-    description
-    lastSeen
-    createdAt
-    createdBy {
-      _id
-      username
-      email
-      address
-    }
   }
 }
 `
