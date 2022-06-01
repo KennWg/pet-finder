0c526f681acb9363daa8ec4e106a4e665fc74e05
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-06-01 14:57:05 -0400

    switch the app.js with the react route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-
-
 
 import './App.css';
 
@@ -10,9 +10,17 @@ import Navbar from './components/Navbar/';
 import Header from './components/Header/Header.js';
 import Main from './components/Main/';
 import Footer from './components/Footer/Footer.js';
+import AllReports from './components/Main/AllReports';
+import CreateReport from './components/Main/CreateReport';
+import Dashboard from './components/Main/Dashboard';
+import Home from './components/Main/Home';
+import Login from './components/Main/Login';
+import LogoutInfo from './components/Main/LogoutInfo';
+import SignUp from './components/Main/SignUp';
+import SingleReport from './components/Main/SingleReport';
 
 const httpLink = createHttpLink({
-// Remember to change back to:   uri: '/graphql'    for deployment
+  // Remember to change back to:   uri: '/graphql'    for deployment
 
   uri: 'http://localhost:3001/graphql'
 });
@@ -39,10 +47,50 @@ function App() {
       <ApolloProvider client={client}>
 
         <StoreProvider>
-          <Navbar />
-          <Header />
-          <Main />
-          <Footer />
+
+          <Router>
+
+            <Navbar />
+            <Header />
+            <Main>
+
+              <Routes>
+                <Route
+                  path="/all_reports"
+                  element={<AllReports />}
+                />
+                <Route
+                  path="/create_report"
+                  element={<CreateReport />}
+                />
+                <Route
+                  path="/dashboard"
+                  element={<Dashboard />}
+                />
+                <Route
+                  path="/login"
+                  element={<Login />}
+                />
+                <Route
+                  path="/logout_info"
+                  element={<LogoutInfo />}
+                />
+                <Route
+                  path="/signup"
+                  element={<SignUp />}
+                />
+                <Route
+                  path="/single_report"
+                  element={<SingleReport />}
+                />
+
+              </Routes>
+
+            </Main>
+            <Footer />
+
+          </Router>
+
         </StoreProvider>
 
       </ApolloProvider >
