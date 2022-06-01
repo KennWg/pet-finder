c2d6f209c3a85b741e060738fb461ae15f4e66db
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-06-01 12:53:07 -0400

    add demo image to the rpt_preview.js

--- client/src/components/Main/SubComponents/RPT_Preview.js
+++ client/src/components/Main/SubComponents/RPT_Preview.js
@@ -1,30 +1,34 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
 import { QUERY_REPORTS_BY_USER_ID } from '../../../utils/queries';
+import rpt_photot1 from '../../../assets/images/RPT_photo/1.jpg'
+import rpt_photot2 from '../../../assets/images/RPT_photo/2.jpg'
+import rpt_photot3 from '../../../assets/images/RPT_photo/3.jpg'
+import rpt_photot4 from '../../../assets/images/RPT_photo/4.jpg'
 
 const fillerData = [
     {
         "_id": "62965ace7f1b0daa38cbf04c",
         "name": "Dawson",
-        "photo": "7059683.jpg",
+        "photo": rpt_photot1,
         "lastSeen": "zvczxcv"
     },
     {
         "_id": "62956de45003468d6aa1746d",
         "name": "Kindric",
-        "photo": "7059683.jpg",
+        "photo": rpt_photot2,
         "lastSeen": "bvcx"
     },
     {
         "_id": "62956da95003468d6aa17469",
         "name": "Holly",
-        "photo": "7059683.jpg",
+        "photo": rpt_photot3,
         "lastSeen": "gfsdfg"
     },
     {
         "_id": "629569293e9aa0dbb50810f8",
         "name": "Charlie",
-        "photo": "7059683.jpg",
+        "photo": rpt_photot4,
         "lastSeen": "Walking in the park near the church at davisville and eglington - a bear"
     }
 ]
