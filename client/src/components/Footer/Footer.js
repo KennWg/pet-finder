e23c44b0ec3f413f9971733047c940e48c136c33
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-06-02 15:34:26 -0400

    working on dashboard layout and functionality

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
 
     <footer className="footer-class">
       <p>
-      &copy; Copyright Error-dite! 2022
+      &copy; Error-dite 2022
       </p>
       <img className="footer-img" src={bucholic} alt="a footer of a bucholic horizon" />
     </footer>
