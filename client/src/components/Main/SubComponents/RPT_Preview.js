d5954a929b13733f3f2284f1e572cb9be788cc1b
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-06-01 22:19:15 -0400

    teaching

--- client/src/components/Main/SubComponents/RPT_Preview.js
+++ client/src/components/Main/SubComponents/RPT_Preview.js
@@ -46,12 +46,12 @@ console.log(allReports);
     return (
     <>
                 {allReports.map((report) => (
-                <a key={report._id} dataValue={report._id} className="reportThumbnail sub-comp-outer-div brand-bg-color">
-                    <div key={report._id} className="rpt-preview-div sub-comp-inner-div">
-                        <img key={report._id} src={report.photo} alt={report.name + ", a lost pet"} />
-                        <div key={report._id} className="rpt-preview-text-div">
-                        <p key={report._id} className="rpt-preview-pet-name">{report.name}</p>
-                        <p key={report._id} className="rpt-preview-last-seen"><strong>Last seen: </strong> {report.lastSeen}</p>
+                <a key={report._id + "anchorTag"} datavalue={report._id} className="reportThumbnail sub-comp-outer-div brand-bg-color">
+                    <div key={report._id + "sub-comp-inner-div"} className="rpt-preview-div sub-comp-inner-div">
+                        <img key={report._id + "img"} src={report.photo} alt={report.name + ", a lost pet"} />
+                        <div key={report._id + "details"} className="rpt-preview-text-div">
+                        <p key={report._id + report.name +"5"} className="rpt-preview-pet-name">{report.name}</p>
+                        <p key={report._id + report.lastSeen + "6"} className="rpt-preview-last-seen"><strong>Last seen: </strong> {report.lastSeen}</p>
                         </div>
                     </div>
                 </a>
