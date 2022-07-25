f7b5688fcaafa6a8d92ca7d32f6aa0173d8a7c70
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-07-25 19:44:15 -0400

    added toggling menu functionality

--- client/src/components/Navbar/Laptop.js
+++ client/src/components/Navbar/Laptop.js
@@ -1,9 +1,45 @@
 import { useStoreContext } from '../../utils/GlobalStore';
+import React, { useState } from 'react';
+import menuIcon from '../../assets/images/nav_menu.png';
 import { UPDATE_VIEW } from '../../utils/actions';
 
 import Auth from '../../utils/auth.js';
+let navbarVisible = false;
 
 function Laptop() {
+
+    const menuClosed = {
+        visibility: "hidden",
+        border: "3px solid yellow"
+    }
+
+    const menuOpen = {
+        visibility: "visible",
+        border: "3px solid blue"
+    }
+
+    console.log("before");
+    const [navbarStyle, setNavbarStyle] = useState(menuClosed);
+
+    console.log(navbarStyle);
+    console.log("after");
+
+
+
+    const toggleNavMenu = () => {
+        console.log("1", navbarVisible);
+
+        navbarVisible = !navbarVisible;
+        console.log("2", navbarVisible);
+
+        (navbarVisible) ? setNavbarStyle(menuOpen) : setNavbarStyle(menuClosed);
+        console.log(navbarStyle);
+        console.log("3", navbarVisible);
+
+    }
+
+
+
 
     const [state, dispatch] = useStoreContext();
     const { currentView, navBarChoices, navBarChoicesNOT } = state;
@@ -13,14 +49,14 @@ function Laptop() {
     }
 
     const handleClick = async clickedNavIcon => {
-        if (clickedNavIcon==="logout_info"){
-            Auth.logout();            
+        if (clickedNavIcon === "logout_info") {
+            Auth.logout();
         }
-        else if(clickedNavIcon==="home"){
-            clickedNavIcon=""
+        else if (clickedNavIcon === "home") {
+            clickedNavIcon = ""
         }
-        
-        window.location.assign('/'+clickedNavIcon);               
+
+        window.location.assign('/' + clickedNavIcon);
     };
 
     let tempArr = [];
@@ -29,22 +65,27 @@ function Laptop() {
         : tempArr = navBarChoicesNOT;
 
     return (
-        <div className="laptop-navbar navbar-div">
-            {tempArr.map((view) => (
-                <div className={`laptop-nav-div`}
-                    key={view.name}>
-                    <span className={`nav-link ${currentView === view.name ? 'active' : ''}`}
-                        onClick={() => {
-                            handleClick(view.name);
-                        }}
-                        // href="#"
-                        title={view.description}>
-                        <img src={imagePath(view.name)} className="laptop-nav-icon" alt={view.name} />
-                    </span>
-                </div>
-            ))
-            }
-        </div >
+        <div>
+            <div className={`laptop-nav-div`} style={{ position: "absolute", top: "1vh", right: "1.6vw" }} onClick={toggleNavMenu}>
+                <img src={menuIcon} className="menu-icon laptop-nav-icon" style={{ border: "3px dotted green" }} />
+            </div>
+            <div className="laptop-navbar navbar-div" style={navbarStyle}>
+                {tempArr.map((view) => (
+                    <div className={`laptop-nav-div`}
+                        key={view.name}>
+                        <span className={`nav-link ${currentView === view.name ? 'active' : ''}`}
+                            onClick={() => {
+                                handleClick(view.name);
+                            }}
+                            // href="#"
+                            title={view.description}>
+                            <img src={imagePath(view.name)} className="laptop-nav-icon" alt={view.name} />
+                        </span>
+                    </div>
+                ))
+                }
+            </div >
+        </div>
     )
 }
 
